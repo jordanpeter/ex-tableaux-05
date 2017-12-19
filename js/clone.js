c923e7fb64bee823c8c858e185a28efbb749504a
jordanpeter <jordanpeter26@gmail.com>
2017-12-19 15:30:09 +0100

    ok

---
+++ js/clone.js
@@ -0,0 +1,5 @@
+var myArray = ['Mario', 'Luigi', 'Peach'];
+var myArray2 = myArray.slice(0,3);
+myArray2.push('Bowser');
+console.log("Voici les élements du tableau sans Bowser: "+myArray);
+console.log("Voici les élements du tableau copié avec ajout de Bowser !\n "+myArray2);
