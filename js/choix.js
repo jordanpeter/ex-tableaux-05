f3d81cf073c7f11d2ed3ddf3349384f7264139d5
jordanpeter <jordanpeter26@gmail.com>
2017-12-19 15:00:15 +0100

    pause

---
+++ js/choix.js
@@ -0,0 +1,6 @@
+var myArray = ['Jackman', 'Stalone', 'Cruiz'];
+var nb = 1;
+     for (var i = 0; i < myArray.length; i++) {
+          console.log("Le numéro "+nb+" est "+myArray[i]);
+          nb++;
+     }
---
+++ js/index.js
@@ -0,0 +1,8 @@
+var tabArray = [1,2,3,4,5,6,7,8,9];
+var result = 0;
+for (var i = 0; i < tabArray.length; i++) {
+
+    result= result + tabArray[i];
+    console.log(result);
+
+}
